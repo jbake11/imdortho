bdaa041e38ac8c0e33cb5bf2dcda6726c6a3e4f7
jbake11 <jeffbake40@gmail.com>
2014-09-11 11:26:02 -0400

    added old/new building pics to history

--- history.php
+++ history.php
@@ -15,14 +15,18 @@ Old & New Location Pics
 
 <div class="container">
     <div class="row">
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://2.bp.blogspot.com/-H6MAoWN-UIE/TuRwLbHRSWI/AAAAAAAABBk/89iiEulVsyg/s400/Free%2BNature%2BPhoto.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://www.virginia.org/uploadedImages/virginiaorg/Images/OrgImages/H/HamptonConventionVisitorBureau/Grandview_Nature_Preserve.jpg?width=300&height=200&scale=upscalecanvas" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://blog.arborday.org/wp-content/uploads/2013/02/NEC1-300x200.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://th03.deviantart.net/fs70/200H/f/2010/256/0/9/painting_of_nature_by_dhikagraph-d2ynalq.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://www.virginia.org/uploadedImages/virginiaorg/Images/OrgImages/H/HamptonConventionVisitorBureau/Grandview_Nature_Preserve.jpg?width=300&height=200&scale=upscalecanvas" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://th03.deviantart.net/fs70/200H/f/2010/256/0/9/painting_of_nature_by_dhikagraph-d2ynalq.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://2.bp.blogspot.com/-H6MAoWN-UIE/TuRwLbHRSWI/AAAAAAAABBk/89iiEulVsyg/s400/Free%2BNature%2BPhoto.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://blog.arborday.org/wp-content/uploads/2013/02/NEC1-300x200.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='First Warsaw Location (5000 sq.ft.)' class="img-responsive" src="/images/building/oldbuild1.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild1.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild4.jpg" /></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='First Warsaw Location (5000 sq.ft.)' class="img-responsive" src="/images/building/oldbuild2.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild2.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild3.jpg" /></div>
+
     </div>
 </div>
 
