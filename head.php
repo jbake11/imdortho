c56544126aefe892b5247387d10727f971af886d
jbake11 <jeffbake40@gmail.com>
2014-09-11 15:45:46 -0400

    We got Nav!

--- head.php
+++ head.php
@@ -16,13 +16,27 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
+    <div class='container-fluid'>
+        <div class='row'>
+            <nav class="navbar navbar-default navbar-static-top" role="navigation">
+                <div class="container">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="history.php">History</a></li>
+                        <li><a href="photo.php">Photo Gallery</a></li>
+                        <li><a href="equip.php">Equipment & Services</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
 
-
-    </head>
-    <body>
-        <div class='container'>
-            <div class='row'>
-                <div class='col-lg-12'>
+</head>
+<body>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-lg-12'>
 
 
 
