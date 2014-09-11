7148644f780d0b792ed4c86c76f29a86a5622789
jbake11 <jeffbake40@gmail.com>
2014-09-11 13:40:55 -0400

    photo gallery with fancy box: all images added

--- photo.php
+++ photo.php
@@ -8,8 +8,8 @@ include("head.php");
 <h2>IMD Specializes in a number of production processes for the Orthopaedic Industry. Below is a gallery
     of products we have manufactured for various clients.</h2>
 
-
 <!--####
+1OK) Add jQuery library "<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>"
 2) Download fancybox (https://github.com/fancyapps/fancyBox)
 3) Add jquery.fancybox.css and jquery.fancybox.js
 4) Add fancybox images
@@ -23,53 +23,169 @@ include("head.php");
     <div class="row">
         <div class='list-group gallery'>
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120036.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120036.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120091.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120091.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120116.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120116.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120128.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120128.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120132.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120132.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
-                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120145.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120145.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120193.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120193.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120197.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120197.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120211.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120211.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120240.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120240.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120284.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120284.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120303.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120303.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120311.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120311.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120323.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120323.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811120323.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811120323.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811170341.jpg23.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811170341.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811170347.jpg23.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811170347.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811170363.jpg23.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811170363.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811170387.jpg23.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811170387.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/811170390.jpg23.jpg">
+                    <img class="img-responsive" alt="" src="/images/gallery/811170390.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'>Image Title</small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+
+
+
+
         </div> <!-- list-group / end -->
     </div> <!-- row / end -->
 </div> <!-- container / end -->
