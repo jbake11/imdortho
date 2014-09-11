4a57e91a8c9844891969410687e964c41c0d7fe7
jbake11 <jeffbake40@gmail.com>
2014-09-11 09:17:15 -0400

    footer bg

--- foot.php
+++ foot.php
@@ -1,4 +1,18 @@
 
+<div id='footer' class="container-fluid" >
+    <div class="row" >
+        <div class='col-lg-3'>
+            <h3>About</h3>
+
+        </div>
+
+        <div class='col-lg-9'>
+            <h3>Contact Us</h3>
+
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>
