49939774e3f6a85e7a8c3138382e40a33c1a3203
jbake11 <jeffbake40@gmail.com>
2014-09-11 12:54:47 -0400

    created new/old building image in PS for history

--- history.php
+++ history.php
@@ -4,37 +4,26 @@ include("head.php");
 ?>
 
 
-
-<h1>Company History</h1>
-
-<!--
-
-Old & New Location Pics
-
--->
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='First Warsaw Location (5000 sq.ft.)' class="img-responsive" src="/images/building/oldbuild1.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild1.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild4.jpg" /></div>
-    </div>
+<div class='container'>
+    <h1>Company History</h1>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='First Warsaw Location (5000 sq.ft.)' class="img-responsive" src="/images/building/oldbuild2.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild2.jpg" /></div>
-        <div class="col-md-3 col-sm-4 col-xs-6"><img alt='Current Warsaw Location (30,000 sq.ft.)' class="img-responsive" src="/images/building/newbuild3.jpg" /></div>
-
+<div class='container'>
+    <div class='row'>
+        <img src='images/building/building.png'
     </div>
 </div>
 
 
-<p>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992 the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana. </p>
-<p>IMD purchased the building and went through 3 additions from 1992 to 2005, at that time IMD¹s building was less than 5000 sq. ft., in 2005 the decision was made to build the facility where we are located at now, our current facility is 12,000 sq ft (9000 sq ft manufacturing, 3000 sq. ft office) with the business growing the decision has been made to add an additional 18,000 sq. ft (12,000 manufacturing, 6,000 office) construction should be complete early 2009. IMD gives all the credit to our customers and employees. </p>
-<p>Instrumental Machine & Development is committed to Customer Satisfaction by providing Quality Products that meet or exceed their expectations, On-Time delivery and Customer Service through engineering support and communication. </p>
 
+
+<div class='container'>
+    <div class='row'>
+        <p>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992 the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana. </p>
+        <p>IMD purchased the building and went through 3 additions from 1992 to 2005, at that time IMD¹s building was less than 5000 sq. ft., in 2005 the decision was made to build the facility where we are located at now, our current facility is 12,000 sq ft (9000 sq ft manufacturing, 3000 sq. ft office) with the business growing the decision has been made to add an additional 18,000 sq. ft (12,000 manufacturing, 6,000 office) construction should be complete early 2009. IMD gives all the credit to our customers and employees. </p>
+        <p>Instrumental Machine & Development is committed to Customer Satisfaction by providing Quality Products that meet or exceed their expectations, On-Time delivery and Customer Service through engineering support and communication. </p>
+    </div>
+</div>
 
 
 <?php
