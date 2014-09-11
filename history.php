a6e4e712bc190d6feb6eebf4d83ea88e625b9f81
jbake11 <jeffbake40@gmail.com>
2014-09-11 10:59:20 -0400

    chopped out old/new building pics in PS
    added img class

--- history.php
+++ history.php
@@ -13,6 +13,20 @@ Old & New Location Pics
 
 -->
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://2.bp.blogspot.com/-H6MAoWN-UIE/TuRwLbHRSWI/AAAAAAAABBk/89iiEulVsyg/s400/Free%2BNature%2BPhoto.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://www.virginia.org/uploadedImages/virginiaorg/Images/OrgImages/H/HamptonConventionVisitorBureau/Grandview_Nature_Preserve.jpg?width=300&height=200&scale=upscalecanvas" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://blog.arborday.org/wp-content/uploads/2013/02/NEC1-300x200.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://th03.deviantart.net/fs70/200H/f/2010/256/0/9/painting_of_nature_by_dhikagraph-d2ynalq.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://www.virginia.org/uploadedImages/virginiaorg/Images/OrgImages/H/HamptonConventionVisitorBureau/Grandview_Nature_Preserve.jpg?width=300&height=200&scale=upscalecanvas" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://th03.deviantart.net/fs70/200H/f/2010/256/0/9/painting_of_nature_by_dhikagraph-d2ynalq.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://2.bp.blogspot.com/-H6MAoWN-UIE/TuRwLbHRSWI/AAAAAAAABBk/89iiEulVsyg/s400/Free%2BNature%2BPhoto.jpg" /></div>
+        <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://blog.arborday.org/wp-content/uploads/2013/02/NEC1-300x200.jpg" /></div>
+    </div>
+</div>
+
+
 <p>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992 the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana. </p>
 <p>IMD purchased the building and went through 3 additions from 1992 to 2005, at that time IMD¹s building was less than 5000 sq. ft., in 2005 the decision was made to build the facility where we are located at now, our current facility is 12,000 sq ft (9000 sq ft manufacturing, 3000 sq. ft office) with the business growing the decision has been made to add an additional 18,000 sq. ft (12,000 manufacturing, 6,000 office) construction should be complete early 2009. IMD gives all the credit to our customers and employees. </p>
 <p>Instrumental Machine & Development is committed to Customer Satisfaction by providing Quality Products that meet or exceed their expectations, On-Time delivery and Customer Service through engineering support and communication. </p>
