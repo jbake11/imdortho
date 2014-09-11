0c5058bde9a658af9a148a4876187b080a299fe1
jbake11 <jeffbake40@gmail.com>
2014-09-11 01:40:49 -0400

    carousel

--- index.php
+++ index.php
@@ -4,8 +4,67 @@ include("head.php");
 ?>
 
 
-echo "some stuff";
+<div class="container">
+    <div class="row">
+        <h2>Carousel with text and transition timer.</h2>
+    </div>
 
+    <div class="row">
+        <!-- The carousel -->
+        <div id="transition-timer-carousel" class="carousel slide transition-timer-carousel" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#transition-timer-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#transition-timer-carousel" data-slide-to="1"></li>
+                <li data-target="#transition-timer-carousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="http://placehold.it/1200x400" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 1</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="http://placehold.it/1200x400/AAAAAA/888888" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 2</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="http://placehold.it/1200x400/888888/555555" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 3</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#transition-timer-carousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#transition-timer-carousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+
+            <!-- Timer "progress bar" -->
+            <hr class="transition-timer-carousel-progress-bar animate" />
+        </div>
+    </div>
+</div>
 
 <?php
 include("foot.php");
