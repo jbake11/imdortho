997d6b0248012fdfd6048570e874d14174735957
jbake11 <jeffbake40@gmail.com>
2014-09-11 01:34:39 -0400

    initial structure

---
+++ head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Instrumental Machine & Development</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/slate/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="/css/main.css">
+
+    </head>
+    <body>
+
+
---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+include("head.php");
+?>
+
+
+echo "some stuff";
+
+
+<?php
+include("foot.php");
+?>
+
+
+
