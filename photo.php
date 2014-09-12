461f994300838fdbd42af701123399e65ac9ac0b
jbake11 <jeffbake40@gmail.com>
2014-09-12 16:51:00 -0400

    <div class='thumb-wrap'>

--- photo.php
+++ photo.php
@@ -28,14 +28,19 @@ include("head.php");
     <div class="row">
         <div class='list-group gallery'>
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/1765-02.jpg">
-                    <img class="img-responsive" alt="" src="/images/gallery/1765-02.jpg" />
-                </a>
+                <div class='thumb-wrap'>
+                    <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/1765-02.jpg">
+                        <img class="img-responsive" alt="" src="/images/gallery/1765-02.jpg" />
+                    </a>
+                </div>
             </div> <!-- col-6 / end -->
+
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/1878.jpg">
-                    <img class="img-responsive" alt="" src="/images/gallery/1878.jpg" />
-                </a>
+                <div class='thumb-wrap'>
+                    <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/1878.jpg">
+                        <img class="img-responsive" alt="" src="/images/gallery/1878.jpg" />
+                    </a>
+                </div>
             </div> <!-- col-6 / end -->
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
@@ -103,6 +108,7 @@ include("head.php");
                     <img class="img-responsive" alt="" src="/images/gallery/Tools-11.jpg" />
                 </a>
             </div> <!-- col-6 / end -->
+
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/Tools-15.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/Tools-15.jpg" />
