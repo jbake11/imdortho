a9235e7212d0e776519fad79ffead7528a074a83
jbake11 <jeffbake40@gmail.com>
2014-09-13 00:13:15 -0400

    gch

--- foot.php
+++ foot.php
@@ -8,16 +8,18 @@
     <div class="row" >
         <div class='col-lg-offset-1 col-md-offset-1 col-lg-3'>
             <a href='history.php'><h3 class="footTitle">About</h3></a>
+            <hr />
             <p class='footText'>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992, the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana.</p>
         </div>
 
         <div class='col-lg-3'>
             <a href='contact.php'><h3 class="footTitle">Contact Us</h3></a>
-            <p class='footText'>INSTRUMENTAL MACHINE & DEVELOPMENT<br>
-                2098 N. Pound Drive, West<br>
-                Warsaw, Indiana 46582<br>
-                <br>
-                Phone: 574-267-7713<br>
+            <hr />
+            <p class='footText'>INSTRUMENTAL MACHINE & DEVELOPMENT<br />
+                2098 N. Pound Drive, West<br />
+                Warsaw, Indiana 46582<br />
+                <br />
+                Phone: 574-267-7713<br />
                 Fax: 574-267-2551</p>
         </div>
     </div>
@@ -31,9 +33,9 @@
             © 2014 Instrumental Machine & Development, Inc. All Rights Reserved.
         </div>
         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 nwrap">
-            <a href="images/ISO_Cert.pdf"><img src='images/pjicon.png'></a>
-            <a href="http://www.anab.org/"><img src='images/anabicon.png'></a>
-            <a href="#"><img src='images/fbicon.png'></a>
+            <a href="#"><img class='pull-right' src='images/fbicon.png'></a>
+            <a href="http://www.anab.org/"><img class='pull-right' src='images/anabicon.png'></a>
+            <a href="images/ISO_Cert.pdf"><img class='pull-right' src='images/pjicon.png'></a>
         </div>
     </div>
 </div>
--- photo.php
+++ photo.php
@@ -1,7 +1,34 @@
 
 <?php
+
 include("head.php");
 ?>
+
+<script type='text/javascript'>
+    jQuery(document).ready(function($) {
+
+        $('#myCarousel').carousel({
+            interval: 5000
+        });
+
+        $('#carousel-text').html($('#slide-content-0').html());
+
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function() {
+            var id_selector = $(this).attr("id");
+            var id = id_selector.substr(id_selector.length - 1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+        });
+
+
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function(e) {
+            var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-' + id).html());
+        });
+    });
+</script>
 
 <div class='container-fluid'>
     <h1>Photo Gallery</h1>
@@ -24,6 +51,85 @@ include("head.php");
 <script src="/js/fancybox.js"></script>
 
 
+<div class="container-fluid">
+    <div id="main_area">
+        <!-- Slider -->
+        <div class="row">
+            <div class="col-xs-12" id="slider">
+                <!-- Top part of the slider -->
+                <div class="row">
+                    <div class="col-sm-12" id="carousel-bounding-box">
+                        <div class="carousel slide" id="myCarousel">
+                            <!-- Carousel items -->
+                            <div class="carousel-inner">
+                                <div class="active item" data-slide-number="0">
+                                    <img src="images/gallery/1765-02.jpg"></div>
+
+                                <div class="item" data-slide-number="1">
+                                    <img src="images/gallery/1878.jpg"></div>
+
+                                <div class="item" data-slide-number="2">
+                                    <img src="images/gallery/4240.jpg"></div>
+
+                                <div class="item" data-slide-number="3">
+                                    <img src="images/gallery/5201_gold.jpg"></div>
+
+                                <div class="item" data-slide-number="4">
+                                    <img src="images/gallery/5211_gold.jpg"></div>
+
+                                <div class="item" data-slide-number="5">
+                                    <img src="images/gallery/5220_gold.jpg"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/Slider-->
+
+        <div class="row hidden-xs" id="slider-thumbs">
+            <!-- Bottom switcher of slider -->
+            <ul class="hide-bullets">
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-0"><img src="images/gallery/1765-02.jpg" alt=""/></a>
+
+                </li>
+
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-1"> <img src="images/gallery/1878.jpg" alt=""/></a>
+                </li>
+
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-2"><img src="images/gallery/4240.jpg" alt=""/></a>
+                </li>
+
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-3"><img src="images/gallery/5201_gold.jpg" alt=""/></a>
+                </li>
+
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-4"><img src="images/gallery/5211_gold.jpg" alt=""/></a>
+                </li>
+
+                <li class="col-sm-2">
+                    <a class="thumbnail" id="carousel-selector-5"><img src="images/gallery/5220_gold.jpg" alt=""/></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<!--<img src="images/gallery/5230_gold.jpg" alt=""/>
+<img src="images/gallery/6620.jpg" alt=""/>
+<img src="images/gallery/6865-04_gold.jpg" alt=""/>
+<img src="images/gallery/7400_handle.jpg" alt=""/>
+<img src="images/gallery/9125.jpg" alt=""/>
+<img src="images/gallery/T1006.jpg" alt=""/>
+<img src="images/gallery/Tools-11.jpg" alt=""/>
+<img src="images/gallery/Tools-15.jpg" alt=""/>
+<img src="images/gallery/aluminum_remnants.jpg" alt=""/>-->
+
+<!--
 <div class="container">
     <div class="row">
         <div class='list-group gallery'>
@@ -33,7 +139,7 @@ include("head.php");
                         <img class="img-responsive" alt="" src="/images/gallery/1765-02.jpg" />
                     </a>
                 </div>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <div class='thumb-wrap'>
@@ -41,89 +147,90 @@ include("head.php");
                         <img class="img-responsive" alt="" src="/images/gallery/1878.jpg" />
                     </a>
                 </div>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/4240.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/4240.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/5201_gold.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/5201_gold.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/5211_gold.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/5211_gold.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/5220_gold.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/5220_gold.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/5230_gold.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/5230_gold.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/6620.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/6620.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/6865-04_gold.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/6865-04_gold.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/7400_handle.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/7400_handle.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/9125.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/9125.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/T1006.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/T1006.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/Tools-11.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/Tools-11.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/Tools-15.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/Tools-15.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
+            </div>  col-6 / end
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="/images/gallery/aluminum_remnants.jpg">
                     <img class="img-responsive" alt="" src="/images/gallery/aluminum_remnants.jpg" />
                 </a>
-            </div> <!-- col-6 / end -->
-        </div> <!-- list-group / end -->
-    </div> <!-- row / end -->
-</div> <!-- container / end -->
-
+            </div>  col-6 / end
+        </div>  list-group / end
+    </div>  row / end
+</div>  container / end
+-->
 
 <?php
+
 include("foot.php");
 ?>
