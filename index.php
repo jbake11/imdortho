7b50477df1f23120b9542a062bbcf9052e4833d7
jbake11 <jeffbake40@gmail.com>
2014-09-15 23:15:06 -0400

    menu overlaps inclusive logo

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ include("head.php");
 </script>
 
 
-<div class='smNav clearfix hidden-lg hidden-md hidden-sm'>
+<div class='underNav clearfix hidden-lg hidden-md hidden-sm'>
     <br /><br />
 </div>
 
