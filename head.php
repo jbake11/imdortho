81be10dc01f10b116a08cdfe0563d239749300c9
jbake11 <jeffbake40@gmail.com>
2014-09-15 22:06:59 -0400

    small nav might be good now

--- head.php
+++ head.php
@@ -51,7 +51,7 @@
         </div>
 
 
-        <div class='container hidden-lg hidden-md'>
+        <div class='container hidden-lg hidden-md hidden-xs'>
             <div class='row'>
                 <div class='col-xs-2 col-sm-2 transp'>
                     <a href="index.php"><img class='image-responsive' src='images/logo.png'></a>
@@ -75,6 +75,38 @@
                     </ul>
                 </div>
             </div>
+        </div>
+
+        <!-- Fixed navbar -->
+        <div class="hidden-lg hidden-md hidden-sm navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="container logoAdjust">
+                        <a class="navbar-brand" href="#"><img src="images/imdlogo-sm.png"></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                            <li><a href="history.php">History</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="photo.php">Photo Gallery</a></li>
+                            <li><a href="equip.php">Equipment & Services</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+        <div class='smNav clearfix hidden-lg hidden-md hidden-sm'>
+            <br /><br />
         </div>
 
         <div id="bodywrap" class='container'>
---
+++ test.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+
+
+
+
+    <head>
+        <title>Industrial Machine &amp; Development</title>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Warsaw, Indiana, orthopaedic, instruments, CNC, milling, marking, laser marking, manufacturing, orthopedic instruments, medical, instrumentation, development, sterilization, trays, operating, tools, imd, instrumental, machine, design">
+        <meta name="description" content="Instrumental Machine &amp; Development (IMD) a quality minded manufacturer of Orthopedic and Medical Instrumentation, supporting the Orthopedic community. Located in Warsaw, Indiana " the="" orthopaedic="" capital="" of="" world."="">
+              <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="/css/main.css" media="screen">
+
+
+    </head>
+    <body>
+
+        <!-- Fixed navbar -->
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="container logoAdjust">
+                        <a class="navbar-brand" href="#"><img src="images/imdlogo-sm.png"></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#about">Contact</a></li>
+                            <li><a href="#contact">History</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="../navbar/">Default</a></li>
+                            <li><a href="../navbar-static-top/">Static top</a></li>
+                            <li><a href="./">Fixed top</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+
+
+            <!-- Bootstrap core JavaScript
+            ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+            <script src="../../dist/js/bootstrap.min.js"></script>
+            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+            <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+
+    </body>
