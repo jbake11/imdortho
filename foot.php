4b19ee55657050a127c61da61b5166a38f5dd726
jbake11 <jeffbake40@gmail.com>
2014-09-17 18:40:51 -0400

    bottom bar static

--- foot.php
+++ foot.php
@@ -3,40 +3,41 @@
 </div>
 </div>
 </div>
+<div class="row-fluid navbar-fixed-bottom">
+    <div id='footer' class="container-fluid image-responsive" >
+        <div class="container">
+            <div class="row" >
+                <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <a href='history.php'><h1 class="footTitle">About</h1></a>
+                    <hr />
+                    <p class='footText'>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992, the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana.</p>
+                </div>
 
-<div id='footer' class="container-fluid image-responsive" >
-    <div class="container">
-        <div class="row" >
-            <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
-                <a href='history.php'><h1 class="footTitle">About</h1></a>
-                <hr />
-                <p class='footText'>Instrumental Machine & Development (IMD) was founded in 1991, originally located in Milford, Indiana. In the fall of 1992, the need for more manufacturing space provided the opportunity for IMD to move to Warsaw, Indiana.</p>
-            </div>
-
-            <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
-                <a href='contact.php'><h1 class="footTitle">Contact Us</h1></a>
-                <hr />
-                <p class='footText'>INSTRUMENTAL MACHINE & DEVELOPMENT<br />
-                    2098 N. Pound Drive, West<br />
-                    Warsaw, Indiana 46582<br />
-                    <br />
-                    Phone: 574-267-7713<br />
-                    Fax: 574-267-2551</p>
+                <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <a href='contact.php'><h1 class="footTitle">Contact Us</h1></a>
+                    <hr />
+                    <p class='footText'>INSTRUMENTAL MACHINE & DEVELOPMENT<br />
+                        2098 N. Pound Drive, West<br />
+                        Warsaw, Indiana 46582<br />
+                        <br />
+                        Phone: 574-267-7713<br />
+                        Fax: 574-267-2551</p>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<div class='container-fluid footerBar'>
-    <div class="row">
-        <div class="footCopyrightText col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 col-lg-8 col-md-8 col-sm-6 col-xs-5">
-            © 2014 Instrumental Machine & Development, Inc. All Rights Reserved.
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 nwrap">
-            <a href="https://www.facebook.com/pages/Instrumental-Machine-Development-Inc/116956151744466"><img class='pull-right' src='images/fbicon.png'></a>
-            <a href="http://www.anab.org/"><img class='pull-right footBadgeSpace' src='images/anabicon.png'></a>
-            <a href="images/ISO_Cert.pdf"><img class='pull-right' src='images/pjicon.png'></a>
+    <div class='container-fluid footerBar'>
+        <div class="row">
+            <div class="footCopyrightText col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 col-lg-8 col-md-8 col-sm-6 col-xs-5">
+                © 2014 Instrumental Machine & Development, Inc. All Rights Reserved.
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 nwrap">
+                <a href="https://www.facebook.com/pages/Instrumental-Machine-Development-Inc/116956151744466"><img class='pull-right' src='images/fbicon.png'></a>
+                <a href="http://www.anab.org/"><img class='pull-right footBadgeSpace' src='images/anabicon.png'></a>
+                <a href="images/ISO_Cert.pdf"><img class='pull-right' src='images/pjicon.png'></a>
+            </div>
         </div>
     </div>
 </div>
